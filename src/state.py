6bbd5b695fc536621fba47e49250a3bbdd88df4b
dev-thiagosilva <thiagosilva977@hotmail.com>
2026-06-09 16:30:56 -0300

    refactor: remove unused graph and state management code for multi-agent chatbot

--- src/state.py
+++
@@ -1,6 +0,0 @@
-from typing import Annotated, TypedDict
-from langgraph.graph.message import add_messages
-
-class AgentState(TypedDict):
-    # The messages list will be updated by adding new messages to the existing ones
-    messages: Annotated[list, add_messages]
